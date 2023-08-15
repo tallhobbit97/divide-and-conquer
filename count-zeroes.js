93d0b426cc8588d3b70d96396e0e08fa2aecaea6
tallhobbit97 <125775467+tallhobbit97@users.noreply.github.com>
2023-08-15 19:22:17 +0000

    divided and conquered

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,20 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let leftIdx = 0;
+    let rightIdx = arr.length-1;
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+        if (middleVal < 1){
+            rightIdx = middleIdx - 1;
+        } else if (middleVal >= 1){
+            leftIdx = middleIdx + 1;
+        }
+    }
+    return arr.length - leftIdx;  
 }
 
+console.log(countZeroes([1,1,1,1,0,0])) // 2
+console.log(countZeroes([1,0,0,0,0])) // 4
+console.log(countZeroes([0,0,0])) // 3
+console.log(countZeroes([1,1,1,1])) // 0
 module.exports = countZeroes
--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,20 @@
-function findFloor() {
-  
+function findFloor(arr, num) {
+    let leftIdx = 0;
+    let rightIdx = arr.length-1;
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+        if (middleVal == num){
+            return num;
+        } else if (middleVal > 0 && arr[middleIdx - 1] <= num && num < middleVal){
+            return arr[middleIdx - 1];
+        } else if (num < arr[mid]){
+            rightIdx = middleIdx - 1;
+        } else {
+            leftIdx = middleIdx + 1;
+        }
+    }
+    return -1
 }
 
 module.exports = findFloor
